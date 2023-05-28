3a03086760c4832eb37f4e1b2e1c52e3772c3b3e
Salomon Laleye <salomonlaleye@gmail.com>
2023-05-28 14:43:21 +0200

    Add gh workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-module.exports = nextConfig
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix,
+  basePath,
+};
+
+module.exports = nextConfig;
